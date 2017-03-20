bec5ac498375271c3f4360c697b12b44ad012ffe
Florian Stegre <florian@stegre.com>
2017-03-20 18:32:00 +0100

    navbar and facebook integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
- devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
+  resources :users, only: [:show]
   resources :trajects, only: [:show, :edit, :update]  do
     resources :stops, only: [:create]
   end
