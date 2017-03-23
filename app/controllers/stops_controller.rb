5e55eae985ad0551ae6546fed876025db1d126aa
Simon De Swarte <simon.de-swarte@hotmail.fr>
2017-03-23 14:20:27 +0100

    Stops request added

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,7 +1,42 @@
 class StopsController < ApplicationController
-def create
-end
+  def create
+    @traject = Traject.find(trajects[:id])
+    @user = User.find(current_user.id)
+    @stop = Stop.new(stop_params)
+    @stop.traject = @traject
+    @stop.user = @user
+    @stop.save
+    flash[:notice] = "Votre demande a bien été transmise."
+    redirect_to root_path
+  end
 
-def show
-end
-end
+  def show
+    @user = User.find(current_user.id)
+    @traject = Traject.find(params[:traject_id])
+    @stops = @traject.stops
+    @remain_seats = @traject.seats
+    @stops.each { |stop| @remain_seats -= 1 if stop.status == "Accepted"}
+
+    @stops = Stop.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@stops) do |stop, marker|
+      marker.lat stop.latitude
+      marker.lng stop.longitude
+    end
+
+    authorize @stops
+  end
+
+  def update
+    stop = Stop.find(params[:id])
+    stop.update(stop_params)
+    authorize stop
+    redirect_to traject_stop_path
+  end
+
+private
+
+    def stop_params
+      params.require(:stop).permit(:occurs_at, :address, :status)
+    end
+  end
