3ad54fb5fb0dec1236f476cb0d7cc2048c2a789e
Simon De Swarte <simon.de-swarte@hotmail.fr>
2017-03-24 11:44:40 +0100

    Attachinary added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :trajects
+  has_attachment :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
