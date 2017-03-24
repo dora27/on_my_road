869cc8a8bd324d82c8ba1c9860871e6de0cffd12
Simon De Swarte <simon.de-swarte@hotmail.fr>
2017-03-24 11:00:59 +0100

    More js

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -49,6 +49,9 @@ private
     gmap_hash = Gmaps4rails.build_markers(stops) do |stop, marker|
       marker.lat stop.latitude
       marker.lng stop.longitude
+      marker.json({ :id => stop.id })
+      marker.infowindow "#{stop.user.first_name} #{stop.user.last_name}"
+
     end
     return gmap_hash
   end
